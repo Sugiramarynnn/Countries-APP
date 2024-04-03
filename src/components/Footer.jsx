7d5962f9c2aa660fa1eb3fc60a311cee429671e1
Sugiramarynnn <sugimarylene@gmail.com>
2024-04-03 10:16:54 +0300

    Worked on the logo, added footer plus added countries

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import LogoIcon from "./Logoicon";
+import Mobilelogo from "./Mobilelogo";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-7 p-4  w-full flex bg-slate-200 items-center justify-evenly ">
+      <div className="w-full max-w-screen-lg flex items-center px-6 justify-between gap-3">
+        <LogoIcon />
+        <Mobilelogo />
+        <p>
+          Copyright &copy;{new Date().getFullYear()}.All right reserved for
+          frontend@shecancode
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
